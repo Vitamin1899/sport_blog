09e2c262f52fbb939d985e310c870a4b1578ef85
Vitamin1899 <vitalimarinich@gmail.com>
2015-10-15 23:40:22 +0300

    initial, create db, remove require turbolinks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SportBlog::Application.config.secret_key_base = '36f11b33719d007e37054bbe2f401a472050b042800ec06f85f63d20b78896ecbfd335ee8c9ca51885fcf902feab9fcf6e26196759f9e0044dfc6ff292506dea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SportBlog::Application.config.session_store :cookie_store, key: '_sport-blog_session'
